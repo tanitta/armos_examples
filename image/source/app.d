a2786f0de84e5f82f69d7c13074c047552782702
tanitta <ttata.trit@gmail.com>
2016-01-09 03:29:15 +0900

    change image example

--- image/source/app.d
+++ image/source/app.d
@@ -1,14 +1,43 @@
 import armos;
 
 class TestApp : ar.BaseApp{
-	ar.Image image;
+	ar.Image imageLena;
+	
+	ar.Image imageDman;
+	
+	ar.Image imageFromArray;
+	
 	void setup(){
-		image = new ar.Image;
-		image.load("lena_std.tif");
+		ar.blendMode = ar.BlendMode.Alpha;
+		
+		imageLena = new ar.Image;
+		imageLena.load("lena_std.tif");
+		
+		imageDman= new ar.Image;
+		imageDman.load("d-man.png");
+		
+		imageFromArray = new ar.Image;
+		ubyte[100*100] array;
+		ar.Vector2i position;
+		auto center = ar.Vector2i(50, 50);
+		foreach (int index, ref pixel; array) {
+			position[0] = index%100;
+			position[1] = index/100;
+			pixel = 255 - cast(ubyte)( ( position-center ).norm*20 );
+		}
+		
+		imageFromArray.setFromAlignedPixels(cast(char*)array.ptr, 100, 100, ar.ColorFormat.Gray);
+		import std.stdio;
+		imageFromArray.bitmap.pixel(0, 0).element(0).writeln;
+		imageFromArray.bitmap.width.writeln;
+		imageFromArray.bitmap.height.writeln;
+		
 	}
 	
 	void draw(){
-		image.draw(0, 0);
+		imageLena.draw(0, 0);
+		imageDman.draw(140, 50);
+		imageFromArray.draw(300, 300);
 	}
 }
 
