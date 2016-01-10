8d6cd121a756ba7b97ebbee26ae177ca31163466
tanitta <ttata.trit@gmail.com>
2016-01-11 07:23:28 +0900

    add shader example

---
+++ shader/data/simple.frag
@@ -0,0 +1,10 @@
+// simple.frag
+#version 130
+
+in vec4 vo_color;
+
+out vec4 fragment_color;
+
+void main(void) {
+  fragment_color = vec4(1, 1, 0, 1);
+}
---
+++ shader/source/app.d
@@ -0,0 +1,33 @@
+import armos, std.stdio, std.math;
+
+class TestApp : ar.BaseApp{
+	auto shader = new ar.Shader;
+	auto camera = new ar.Camera();
+	float c = 0;
+	void setup(){
+		shader.load("simple");
+		camera.position = ar.Vector3f(0, 0, -40);
+		camera.target= ar.Vector3f(0, 0, 0);
+		
+	}
+	
+	void update(){
+		c += 1;
+		camera.position = ar.Vector3f(20.0*cos(c*0.05), 20.0*sin(c*0.05), -40.0);
+	}
+	
+	void draw(){
+		shader.begin;
+			camera.begin;
+			ar.pushMatrix;
+			ar.setColor(ar.Color(0xFF1F37));
+			ar.boxPrimitive(
+					ar.Vector3f(0, 0, 0),
+					ar.Vector3f(20, 20, 20)
+			).drawFill();
+			camera.end;
+		shader.end;
+	}
+}
+
+void main(){ar.run(new TestApp);}
